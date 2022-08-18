dc64e49b4cdf679ba7e522b938da1b215ad4f7dc
ytaoCrow <kafka880407@gmail.com>
2022-08-18 20:30:29 +0800

    解決 CROS get 不到 /:id 的問題

--- src/main/java/com/example/springwebservice/controller/UserController.java
+++ src/main/java/com/example/springwebservice/controller/UserController.java
@@ -11,38 +11,42 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/api/v1/user")
+
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @GetMapping()
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/api/v1/user")
     public List<User> getAllUsers() {
         List<User>response=this.userService.getUserList();
         return response;
     }
     //@PathVariable 唯一識別
-    @GetMapping("/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/api/v1/user/{id}")
     public User getUser(@PathVariable int id) {
         User response=this.userService.getUserById(id);
         return response;
     }
 
-    @PostMapping()
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("/api/v1/user")
     public StatusResponse createUser(@RequestBody CreateUserRequest request) {
         String response=(this.userService.createUser(request));
         return new StatusResponse(response);
     }
 
-    @PutMapping("/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PutMapping("/api/v1/user/{id}")
     public StatusResponse updateUser(@PathVariable int id,@RequestBody UpdateUserRequest request) {
         String response=this.userService.updateUser(id,request);
         return new StatusResponse(response);
     }
 
-    @DeleteMapping("/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @DeleteMapping("/api/v1/user/{id}")
     public StatusResponse deleteUser(@PathVariable int id) {
         String reponse =this.userService.deleteUser(id);
 
